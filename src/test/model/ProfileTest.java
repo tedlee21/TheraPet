b24366133ad833b95eafcfc5b4539a03a8a2ccd7
tedlee <jwnl92@ugrad.cs.ubc.ca>
2021-10-11 16:50:48 -0700

    100% Test Coverage for ProfileTest class

--- src/test/model/ProfileTest.java
+++ src/test/model/ProfileTest.java
@@ -1,9 +1,74 @@
 package model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProfileTest {
+    private Profile user;
+
+    @Test
+    public void makeUserZero() {
+        user = new Profile("Joe", -1);
+        assertEquals(0, user.getBalance());
+    }
+
+    @Test
+    public void testGetName() {
+        user = new Profile("Bob", 5);
+        assertEquals("Bob", user.getName());
+    }
+
+    @Test
+    public void testGetNameNumbers() {
+        user = new Profile("25th Bob", 5);
+        assertEquals("25th Bob", user.getName());
+    }
+
+    @Test
+    public void testGetBalance() {
+        user = new Profile("Joe", 5);
+        assertEquals(5, user.getBalance());
+    }
+
+    @Test
+    public void testGetBalanceZero() {
+        user = new Profile("Broke Joe", 0);
+        assertEquals(0, user.getBalance());
+    }
+
+    @Test
+    public void testGetBalanceLot() {
+        user = new Profile("Stacked Joe", 10000);
+        assertEquals(10000, user.getBalance());
+    }
+
+    @Test
+    public void testAddBal() {
+        user = new Profile("Joe", 5);
+        user.addBalance(10);
+        assertEquals(15, user.getBalance());
+    }
+
+    @Test
+    public void testAddBalNone() {
+        user = new Profile("Joe", 5);
+        user.addBalance(0);
+        assertEquals(5, user.getBalance());
+    }
+
+    @Test
+    public void testSubBal() {
+        user = new Profile("Joe", 10);
+        user.subBalance(5);
+        assertEquals(5, user.getBalance());
+    }
+
+    @Test
+    public void testSubBalNone() {
+        user = new Profile("Joe", 10);
+        user.subBalance(0);
+        assertEquals(10, user.getBalance());
+    }
+
 }
