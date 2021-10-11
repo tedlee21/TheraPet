9ece13a6538aa8a916aec2c38abc81fd9a479be5
tedlee <jwnl92@ugrad.cs.ubc.ca>
2021-10-11 16:57:56 -0700

    100% Test Coverage for PetTest class

--- src/test/model/PetTest.java
+++ src/test/model/PetTest.java
@@ -6,5 +6,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PetTest {
-    // delete or rename this class!
+    private Pet myPet;
+
+    @BeforeEach
+    public void runBefore() {
+        myPet = new Pet("Rufus", "Dog");
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Rufus", myPet.getPetName());
+    }
+
+    @Test
+    public void testGetNameNumbers() {
+        myPet = new Pet("50 Big Mac", "Cat");
+        assertEquals("50 Big Mac", myPet.getPetName());
+    }
+
+    @Test
+    public void testGetType() {
+        assertEquals("Dog", myPet.getPetType());
+    }
+
+    @Test
+    public void testGetTypeCat() {
+        myPet = new Pet("50 Big Mac", "Cat");
+        assertEquals("Cat", myPet.getPetType());
+    }
 }
